71a2614b0eab2ed6a94a148b0b18659cba5accf2
Brian Lechthaler <brianlechthaler@protonmail.com>
2023-12-01 17:02:50 -0800

    ✨Create Repository✨

---
+++ model_downloader/main.py
@@ -0,0 +1,17 @@
+from huggingface_hub import snapshot_download
+from os import symlink, environ
+from getpass import getuser
+
+
+hardcoded_version: str = '1c86eca4355ecc3e955a70198a7067da7858cec6'
+hardcoded_model: str = 'Llama-2-70b-chat'
+hardcoded_model_author: str = 'meta-llama'
+
+
+snapshot_download(
+    revision=hardcoded_version,
+    repo_id=f"{hardcoded_model_author}/{hardcoded_model}",
+    token=environ['HF_TOKEN'],
+    local_dir="/opt/model",
+    local_dir_use_symlinks=False
+)
