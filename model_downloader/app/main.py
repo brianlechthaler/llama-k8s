14e1073eaa506389ee15379ee220068263989a25
Brian Lechthaler <brianlechthaler@protonmail.com>
2023-12-03 17:21:04 -0800

    🔧Fine-tune model_downloader main.py🔧: Enable resuming downloads in the downloader script.

--- model_downloader/app/main.py
+++ model_downloader/app/main.py
@@ -11,5 +11,6 @@ snapshot_download(
     repo_id=f"{hardcoded_model_author}/{hardcoded_model}",
     token=environ['HF_TOKEN'],
     local_dir="/opt/model",
-    local_dir_use_symlinks=False
+    local_dir_use_symlinks=False,
+    resume_download=True
 )
