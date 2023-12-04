0f12a69d2734521ee6405e250e495240312f48cd
Brian Lechthaler <brianlechthaler@protonmail.com>
2023-12-03 18:07:31 -0800

    🔧Fine tune model downloader main.py🔧: Adhere to conventions for writing a Python script.

--- model_downloader/app/main.py
+++ model_downloader/app/main.py
@@ -1,17 +1,25 @@
 from huggingface_hub import snapshot_download
 from os import environ
 
-hardcoded_version: str = '1c86eca4355ecc3e955a70198a7067da7858cec6'
-hardcoded_model: str = 'Llama-2-70b-chat'
-hardcoded_model_author: str = 'meta-llama'
+
+def download_model(version, model, author):
+    print(f"Downloading {author}/{model}@{version}")
+    snapshot_download(
+        revision=version,
+        repo_id=f"{author}/{model}",
+        token=environ['HF_TOKEN'],
+        local_dir="/opt/model",
+        cache_dir='/opt/model_cache',
+        local_dir_use_symlinks=False,
+        resume_download=True
+    )
 
 
-snapshot_download(
-    revision=hardcoded_version,
-    repo_id=f"{hardcoded_model_author}/{hardcoded_model}",
-    token=environ['HF_TOKEN'],
-    local_dir="/opt/model",
-    cache_dir='/opt/model_cache',
-    local_dir_use_symlinks=False,
-    resume_download=True
-)
+if __name__ == '__main__':
+    hardcoded_version: str = '1c86eca4355ecc3e955a70198a7067da7858cec6'
+    hardcoded_model: str = 'Llama-2-70b-chat'
+    hardcoded_model_author: str = 'meta-llama'
+
+    download_model(hardcoded_version,
+                   hardcoded_model,
+                   hardcoded_model_author)
