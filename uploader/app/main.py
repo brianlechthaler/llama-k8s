20354b42c249b47aeb76874defbde416b9e3daf2
Brian Lechthaler <brianlechthaler@protonmail.com>
2023-12-07 18:06:20 -0800

    🔧Optimize uploader🔧: Initialize S3 client outside of function definition.

--- uploader/app/main.py
+++ uploader/app/main.py
@@ -1,15 +1,15 @@
 from boto3 import client
 from os import environ
 
-def create_bucket():
-    s3_client = client(
-        's3',
-        aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'],
-        endpoint_url=environ['ENDPOINT_URL'],
-        region_name='default'
-    )
+s3_client = client(
+    's3',
+    aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'],
+    endpoint_url=environ['ENDPOINT_URL'],
+    region_name='default'
+)
 
+def create_bucket():
     response = s3_client.create_bucket(Bucket=environ['BUCKET_NAME'])
     print(f"Bucket created: {response}")
 
