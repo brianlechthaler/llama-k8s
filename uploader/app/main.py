095d4f685533f8a7b8a115f8cb62a65e1bc298e3
Brian Lechthaler <brianlechthaler@protonmail.com>
2023-12-07 18:34:59 -0800

    🔧Optimize uploader🔧: Completely refactor script, adhering to Python conventions and making things more readable.

--- uploader/app/main.py
+++ uploader/app/main.py
@@ -2,27 +2,31 @@ from boto3 import client
 from os import environ
 
 
-s3_client = client(
-    's3',
-    aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'],
-    endpoint_url=environ['ENDPOINT_URL'],
-    region_name='default'
-)
+class Uploader:
+    def __init__(self):
+        self.s3_client = client(
+            's3',
+            aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
+            aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'],
+            endpoint_url=environ['ENDPOINT_URL'],
+            region_name='default'
+            )
 
+    def create_bucket(self):
+        response = self.s3_client.create_bucket(Bucket=environ['BUCKET_NAME'])
+        print(f"Bucket created: {response}")
 
-def create_bucket():
-    response = s3_client.create_bucket(Bucket=environ['BUCKET_NAME'])
-    print(f"Bucket created: {response}")
+    def upload_file(self):
+        response = self.s3_client.upload_file(environ['FILE_PATH'], environ['BUCKET_NAME'], environ['FILE_NAME'])
+        print(f"Uploaded file: {response}")
 
-
-def upload_file():
-    response = s3_client.upload_file(environ['FILE_PATH'], environ['BUCKET_NAME'], environ['FILE_NAME'])
-    print(f"Uploaded file: {response}")
+    def cli_handler(self, operation):
+        if operation == 'create_bucket':
+            self.create_bucket()
+        if operation == 'upload_file':
+            self.upload_file()
 
 
 if __name__ == '__main__':
-    if environ['OPERATION'] == 'create_bucket':
-        create_bucket()
-    if environ['OPERATION'] == 'upload_file':
-        upload_file()
+    runtime = Uploader()
+    runtime.cli_handler(environ['OPERATION'])
