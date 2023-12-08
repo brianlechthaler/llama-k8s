d6c03ca304283f0405a453d20494a5b7f3df9d91
Brian Lechthaler <brianlechthaler@protonmail.com>
2023-12-07 18:19:33 -0800

    🔧Add function to uploader🔧: Add a function to upload files.

--- uploader/app/main.py
+++ uploader/app/main.py
@@ -1,6 +1,7 @@
 from boto3 import client
 from os import environ
 
+
 s3_client = client(
     's3',
     aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
@@ -9,10 +10,19 @@ s3_client = client(
     region_name='default'
 )
 
+
 def create_bucket():
     response = s3_client.create_bucket(Bucket=environ['BUCKET_NAME'])
     print(f"Bucket created: {response}")
 
+
+def upload_file():
+    response = s3_client.upload_file(environ['FILE_PATH'], environ['BUCKET_NAME'], environ['FILE_NAME'])
+    print(f"Uploaded file: {response}")
+
+
 if __name__ == '__main__':
     if environ['OPERATION'] == 'create_bucket':
         create_bucket()
+    if environ['OPERATION'] == 'upload_file':
+        upload_file()
