d2e038de19e9d61c7a28138eb2baba8426f059db
Brian Lechthaler <brianlechthaler@protonmail.com>
2023-12-07 17:30:12 -0800

    🔧Fix uploader🔧: Use a custom script instead of command line tools.

---
+++ uploader/app/main.py
@@ -0,0 +1,18 @@
+from boto3 import client
+from os import environ
+
+def create_bucket():
+    s3_client = client(
+        's3',
+        aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'],
+        endpoint_url=environ['ENDPOINT_URL'],
+        region_name='default'
+    )
+
+    response = s3_client.create_bucket(Bucket=environ['BUCKET_NAME'])
+    print(f"Bucket created: {response}")
+
+if __name__ == '__main__':
+    if environ['OPERATION'] == 'create_bucket':
+        create_bucket()
